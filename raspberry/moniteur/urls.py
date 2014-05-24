337451b9f77291a78b5c99ae0a2401220b2f22e4
Ange BOUABLE <angebouabre@gmail.com>
2014-05-24 14:22:05 +0200

    Update: Implementing New page capteur detail with urls mechanisms

--- raspberry/moniteur/urls.py
+++ raspberry/moniteur/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from moniteur.views import EntrepotDetailView, LocalisationView, CarteDetailView # TemperatureDetail
+from moniteur.views import EntrepotDetailView, LocalisationView, CapteurDetailView, CarteDetailView # TemperatureDetail
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
 
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
    url(r'^localisation/$', LocalisationView.as_view(), name='localisation'), 
    url(r'^localisation/(?P<localisation>[-_.\w]+)/$', EntrepotDetailView.as_view(), name='entrepot-detail'), 
+   url(r'^capteur/(?P<slug>[-_.\w]+)/$', CapteurDetailView.as_view(), name='capteur-detail'), 
    url(r'^carte/(?P<slug>[-_.\w]+)/$', CarteDetailView.as_view(), name='carte-detail'), 
    #url(r'^$', TemperatureList.as_view(), name='temperatures'),
    # url(r'^temperature/(?P<pk>\d+)/$', TemperatureDetail.as_view(), name='temperature'),
--- raspberry/moniteur/views.py
+++ raspberry/moniteur/views.py
@@ -29,19 +29,39 @@ class EntrepotDetailView(ListView):
         capteurs = Capteur.objects.filter(localisation=localisation)
         
         mesures = Mesure.objects.filter(capteur__in=capteurs)
+        
         for capteur in capteurs:
             capteur.mesures = mesures.filter(capteur=capteur)
-            stats = capteur.mesures.aggregate(Avg('valeur'), Max('valeur'), Min('valeur'))
-            capteur.mes_moy = stats['valeur__avg']
-            capteur.mes_max = stats['valeur__max']
-            capteur.mes_min = stats['valeur__min']
+            capteur.last_mesure = capteur.mesures.last()
             print capteur, capteur.type_mesure
             print capteur.mesures 
-            print stats
-             
+        
         context['capteurs'] = capteurs 
         context['localisation'] = localisation 
         return context
+
+
+class CapteurDetailView(DetailView):
+    model = Capteur
+    context_object_name = 'capteur'
+    template_name = 'capteur-detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CapteurDetailView, self).get_context_data(**kwargs)
+        
+        capteur = self.kwargs['slug']
+        capteur = Capteur.objects.filter(slug=capteur)
+        
+        mesures = Mesure.objects.filter(capteur=capteur)
+        stats = mesures.aggregate(Avg('valeur'), Max('valeur'), Min('valeur'))
+        
+        capteur.mes_moy = stats['valeur__avg']
+        capteur.mes_max = stats['valeur__max']
+        capteur.mes_min = stats['valeur__min']
+
+        return context
+       
+
 
 
 class CarteDetailView(DetailView):
